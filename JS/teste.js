7f72d1a0c09eb032194367d290ac4a65ce0aec24
Lab4-Aluno5 <orestemeduarda@gmail.com>
2022-03-07 11:42:37 -0300

    Enviar

---
+++ JS/teste.js
@@ -0,0 +1,18 @@
+/*var nome
+let nome2 = 'Bruno'
+const pi = 3.14*/
+
+//alert('Olá Mundo XD')
+//confirm('Deseja continuar?')
+//let numero = Number((prompt('Insira o primeiro valor: ')))
+//let numero2 = Number((prompt('Insira o segundo  valor: ')))
+//let result =  numero + numero2
+
+//alert(result)
+
+//let teste = true
+//if (teste) {
+//    alert('É verdadeiro')
+//} else{
+//  /  alert('É falso')
+//}
